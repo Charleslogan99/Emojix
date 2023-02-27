fb35ebc80dafc268b3b27cea68516e5258a6439b
Charleslogan <charlesokoro15@gmail.com>
2023-02-27 20:03:10 +0100

    another commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,63 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+let [data, setData] = useState([])
+let [search, setSearch] = useState('')
+
+useEffect(() => {
+fetch('https://emoji-api.com/emojis?access_key=dbdc63a3ca5aface144271d45ba7d90b22c8ffad')
+.then(res => res.json())
+.then(res => setData(res))
+}, [])
+
+let handleSearch = (e)=> {
+  setSearch(e.target.value)
+}
+
+let handleSubmit = ()=> {
+if(search !== '') {
+  fetch('https://emoji-api.com/emojis?search=food&access_key=dbdc63a3ca5aface144271d45ba7d90b22c8ffad')
+.then(res => res.json())
+.then(res => { 
+  if (res) {
+    setData(res)
+  }else{
+    setData([])
+  }
+}
+)
+}
+}
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Hello world</h1>
+       <h1>Emojix</h1>
+       <p className="header-text">A simple emoji search tool</p>
+      
+       <div className="search">
+       <input type="search" placeholder="search for emoji..." className="search-bar"  value={search} onChange={(e)=>handleSearch(e)}/>
+       <button className="" onClick={(e)=>handleSubmit()}><ion-icon name="search"></ion-icon></button>
+      </div>
       </header>
+      <div className="container">
+
+      {
+        data.map((e, i) => 
+        <div className="card" key={e.slug}>
+          <p className="smiley">{e.character}</p>
+          <span className="smiley-name">{e.unicodeName}</span>
+        </div>
+        )
+      }
+
+        
+       
+      </div>
+      
     </div>
+    
   );
 }
 
